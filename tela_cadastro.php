903af1062cd611ea9c83ddfb874ce2978b72a7f4
Daniel Camargo <daniel.camargodelima2@gmail.com>
2025-03-13 11:58:21 -0300

    começando a tela de cadastro de usuario

--- tela_cadastro.php
+++ tela_cadastro.php
@@ -1,11 +1,12 @@
 <?php
 include './includes/header.php';
 ?>
+<html lang="pt-br">
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
-<link rel="stylesheet" href="./assets/css/tela-login.css">
+<link rel="stylesheet" href="./assets/css/tela-cadastro.css">
 
 <section id="tela-login">
     <main class="container-login">
@@ -27,11 +28,9 @@ include './includes/header.php';
                             <input type="checkbox" name="checkbox-termos" class="checkbox-termos">
                             <label>Você concorda com os Termos de Uso e Políticas de privacidade da Crazy Sneakers?</label>
                         </div>
-                        <input type="submit" value="Entrar" class="btn-entrar">
                     </form>
                     <div class="btn-registrar">
-                        <p>Não tem uma conta? Registre-se</p>
-                        <a href="tela_cadastro.php" class="btn btn-primary">Registrar-se</a>
+                        <a href="#" class="btn btn-primary">Registrar-se</a>
                     </div>
                 </div>
             </div>
