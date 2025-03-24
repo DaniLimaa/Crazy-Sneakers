739132cdf7d556504ed8e1505209155612a3dcc9
Daniel Camargo <daniel.camargodelima2@gmail.com>
2025-03-24 11:49:53 -0300

    começando a tela de produto

--- includes/marcas.php
+++ includes/marcas.php
@@ -2,19 +2,19 @@
     <h1 class="titulo-marcas">Marcas</h1>
     <main class="container-marcas">
         <figure>
-            <img src="./assets/img/marcas/nike.png" alt="logo da nike">
+            <a href="#"><img src="./assets/img/marcas/nike.png" alt="logo da nike"></a>
         </figure>
         <figure>
-            <img src="./assets/img/marcas/adidas.png" alt="logo da adidas">
+            <a href="#"><img src="./assets/img/marcas/adidas.png" alt="logo da adidas"></a>
         </figure>
         <figure>
-            <img src="./assets/img/marcas/puma.png" alt="logo da puma" class="logo-puma">
+            <a href="#"><img src="./assets/img/marcas/puma.png" alt="logo da puma" class="logo-puma"></a>
         </figure>
         <figure>
-            <img src="./assets/img/marcas/vans.png" alt="logo da vans">
+            <a href="#"><img src="./assets/img/marcas/vans.png" alt="logo da vans"></a>
         </figure>
         <figure>
-            <img src="./assets/img/marcas/new-balance.png" alt="logo da new balance" class="logo-new-balance">
+            <a href="#"><img src="./assets/img/marcas/new-balance.png" alt="logo da new balance" class="logo-new-balance"></a>
         </figure>
     </main>
 </section>
--- includes/modelos.php
+++ includes/modelos.php
@@ -1,29 +1,29 @@
 <section id="modelos">
-        <h1 class="titulo-modelos">Modelos</h1>
-        <main class="container-modelos">
-            <figure>
-                <img src="./assets/img/modelos/adidas-campus.png" alt="modelo do adidas campus">
+    <h1 class="titulo-modelos">Modelos</h1>
+    <main class="container-modelos">
+        <figure>
+            <a href="#"><img src="./assets/img/modelos/adidas-campus.png" alt="modelo do adidas campus"></a>
                 <figcaption>
                     <h2>Campus 00s</h2>
                 </figcaption>
-            </figure>
-            <figure>
-                <img src="./assets/img/modelos/nike-dunk.png" alt="modelo do nike dunk">
+        </figure>
+        <figure>
+            <a href="#"><img src="./assets/img/modelos/nike-dunk.png" alt="modelo do nike dunk"></a>
                 <figcaption>
                     <h2>Dunk Low</h2>
                 </figcaption>
-            </figure>
-            <figure>
-                <img src="./assets/img/modelos/vans-knu.png" alt="modelo do vans knu skool">
+        </figure>
+        <figure>
+            <a href="#"><img src="./assets/img/modelos/vans-knu.png" alt="modelo do vans knu skool"></a>
                 <figcaption>
                     <h2>Knu Skool</h2>
                 </figcaption>
-            </figure>
-            <figure>
-                <img src="./assets/img/modelos/nike-tn.png" alt="modelo do nike air max plus">
+        </figure>
+        <figure>
+            <a href="#"><img src="./assets/img/modelos/nike-tn.png" alt="modelo do nike air max plus"></a>
                 <figcaption>
                     <h2>Air Max Plus</h2>
                 </figcaption>
-            </figure>
-        </main>
-    </section>
+        </figure>
+    </main>
+</section>
